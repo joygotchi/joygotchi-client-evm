35dbc567decd7cf00551d2b5d80ee3337007b2bf
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-04-26 09:24:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -29,21 +29,18 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    CHAIN_ID: 314159,
-    CHAIN_NAME: "Filecoin - Calibration testnet",
-    CHAIN_NETWORK: "fevm",
-    TOKEN: "tFIL",
-    SYMBOL: "tFIL",
-    RPC: "https://api.calibration.node.glif.io/",
-    URL_FAUCET: "https://faucet.calibnet.chainsafe-fil.io/funds.html",
-    NFT_ADDRESS: '0x9b57e2E0717E39a107c2F80B9165c49B2089835C',
-    TOKEN_ADDRESS: '0x1fb0D724A433832f9f05929139A9D1af065F0B8B',
-    FAUCET_ADDRESS: '0xd350B81931f65f95E76BF7D944236d50021e68DD',
-    DAO_ADDRESS: '0x410EaA07644593d428568eA1B6b435e6f6Ad3C4D',
-    EXPLORER_URL: 'https://calibration.filscan.io',
-    HOST: 'http://localhost:3000',
-    MONGODB_USER:"kurodenjiro",
-    MONGODB_PASSWORD:"potguardian"
+        CHAIN_ID: 1891,
+        CHAIN_NAME: "Lightlink Pegasus Testnet",
+        CHAIN_NETWORK: "Pegasus",
+        TOKEN: "ETHEREUM",
+        SYMBOL: "ETH",
+        RPC: "https://replicator.pegasus.lightlink.io/rpc/v1",
+        URL_FAUCET: "https://faucet.pegasus.lightlink.io/",
+        NFT_ADDRESS:'0x32f93910803B544C4c58240cc5E7B94678A4BD57',
+        TOKEN_ADDRESS:'0xc9D2EDF36Ed48Cc3110c2D833bAc3420df12Ba5a',
+        FAUCET_ADDRESS:'0xCC5D4595ec07e570d00ad7eF3213062071E5E00f',
+        EXPLORER_URL:'https://pegasus.lightlink.io',
+        HOST: 'https://joy-dragon-v2.vercel.app/'
   },
 }
 module.exports = nextConfig
